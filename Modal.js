50ab1794a12d52c2b021cb23a7c50ccef38a2d9b
vlada sana <hello.vladasana@gmail.com>
2021-04-22 14:05:49 +0300

    Timur added Modal and Form

---
+++ Modal.js
@@ -0,0 +1,45 @@
+class Modal {
+    constructor() {
+        this.elements = {
+            parent: document.querySelector('body'),
+            self: document.createElement('div'),
+            select: document.createElement('select'),
+            makeChoice: document.createElement('option'),
+            cardiologist: document.createElement('option'),
+            dentist: document.createElement('option'),
+            therapist: document.createElement('option')
+        }
+    }
+
+    elementCreater() {
+        const {self, select, makeChoice, cardiologist, dentist, therapist} = this.elements;
+        self.classList.add(`select-wrapper`)
+        select.classList.add(`choice-doctor__list`)
+        cardiologist.classList.add(`cardiologist-option`)
+        dentist.classList.add(`dentist-option`)
+        therapist.classList.add(`therapist-option`)
+
+        cardiologist.textContent = 'Кардиолог'
+        dentist.textContent = 'Стоматолог'
+        therapist.textContent = 'Терапевт'
+        makeChoice.textContent = 'Make a choice!'
+    }
+
+    async render() {
+        const {parent, self, select, makeChoice, cardiologist, dentist, therapist} = this.elements;
+        this.elementCreater()
+
+        select.addEventListener('change', (e) => {
+            const form = new Form(e.target.value, self)
+            form.render()
+        })
+
+        select.append(makeChoice, cardiologist, dentist, therapist)
+        self.append(select)
+        parent.append(self)
+    } 
+}
+
+
+const modal = new Modal;
+modal.render();
