108885319b160da9ae72502f84e5fdecb1cee06c
vlada sana <hello.vladasana@gmail.com>
2021-04-25 23:05:07 +0300

    Adjusted filter

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+
+
 // import LoginForm from "./LoginForm.js";
 //
 // const login = new LoginForm(document.querySelector('.container'));
 // login.render()
 
+import LoginForm from "./LoginForm.js";
+import FormFilter from "./FilterForm.js";
+
+const  filter = new FormFilter(document.body);
+filter.render();
+
+
 
 import Header from "./Header.js";
 
 const header = new Header();
+
 header.render();
