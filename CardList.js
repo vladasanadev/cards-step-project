deb0c0db45d105800ecb47d2c2bceb239dd1a28c
Andrey Kotsyk <titan777ukr@gmail.com>
2021-04-27 16:51:54 +0300

    try to fix getAllCards

--- CardList.js
+++ CardList.js
@@ -3,23 +3,26 @@ import API from "./API.js";
 export default class CardList {
     getAllCards(){
         if(localStorage.getItem("token")){
-            console.log(this.getAllCards());
+            console.log(API.getAllCards());
         }
     }
     render(parent, ulClassName, liClassName, allCards) {
-         parent = document.querySelector(parent);
-         allCards.forEach(element => {
-             let ul = document.createElement('ul');
-             ul.className = `${ulClassName}`;
-             for (let prop in element) {
-                 let li = document.createElement('li');
-                 li.className = `${liClassName}`;
-                 li.textContent = element[prop];
-                 ul.append(li);
-                 parent.append(ul);
-             }
-         });
+        parent = document.querySelector(parent);
+        allCards.forEach(element => {
+            let ul = document.createElement('ul');
+            ul.className = `${ulClassName}`;
+            for (let prop in element) {
+                let li = document.createElement('li');
+                li.className = `${liClassName}`;
+                li.textContent = element[prop];
+                ul.append(li);
+                parent.append(ul);
+            }
+        });
     }
 }
-const res = new CardList();
-res.render("body", "list", "list_elem", res.getAllCards);
+let res = new CardList();
+console.log(res.getAllCards());
+res.render("body", "list", "list_elem", API.getAllCards);
+
+// my token 0e17788a-b880-40af-9aeb-7704e484d022
